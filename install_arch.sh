0c8a154c094d30bd6c6faf2be0cf453ece22082d
dabowski <52606353+dabowski@users.noreply.github.com>
2021-05-24 15:40:10 +0200

    My scripts and vimrc

---
+++ install_arch.sh
@@ -0,0 +1,5 @@
+sudo pacman -Sy
+sudo pacman --needed base-devel
+sudo pacman python-pip python-virtualenv nodejs npm openjdk r neovim neovim-qt curl qget gimp mpv firefox
+
+echo "Install RStudio, IntelliJ and Tor Browser"
---
+++ install_debian.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install gcc python3 python3-pip python3-virtualenv nodejs npm default-jdk r-base neovim neovim-qt curl wget gimp mpv firefox-esr
+
+
+echo "Install RStudio, IntelliJ and Tor Browser"
