1ef91321999732f58fa34249429c66c4f35c39b0
dabowski <ukrytytomek@gmail.com>
2021-05-24 17:10:24 +0200

    Adds link to theme

--- install_arch.sh
+++ install_arch.sh
@@ -6,5 +6,11 @@ curl -sLf https://spacevim.org/install.sh | bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 git clone https://aur.archlinux.org/rstudio-desktop-bin.git
 
+mkdir .themes
+mkdir .icons
+
 echo "IntelliJ IDEA:  https://www.jetbrains.com/idea/download/#section=linux"
 echo "Tor Browser:  https://www.torproject.org/download/"
+echo "Papirus:  https://www.xfce-look.org/p/1166289/"
+echo "Mojave dark: https://www.xfce-look.org/p/1275087/"
+echo "Mojave cursors: https://www.xfce-look.org/p/1355701/"
--- install_debian.sh
+++ install_debian.sh
@@ -4,7 +4,13 @@ sudo apt install gcc python3 python3-pip python3-virtualenv nodejs npm default-j
 curl -sLf https://spacevim.org/install.sh | bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
+mkdir .themes
+mkdir .icons
+
 echo "RStudio:  https://www.rstudio.com/products/rstudio/download/#download"
 
 echo "IntelliJ IDEA:  https://www.jetbrains.com/idea/download/#section=linux"
 echo "Tor Browser:  https://www.torproject.org/download/"
+echo "Papirus:  https://www.xfce-look.org/p/1166289/"
+echo "Mojave dark: https://www.xfce-look.org/p/1275087/"
+echo "Mojave cursors: https://www.xfce-look.org/p/1355701/"
