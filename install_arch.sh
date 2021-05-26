0999c84a6b699c838d05e452a83ae6a8da5f2e0d
dabowski <52606353+dabowski@users.noreply.github.com>
2021-05-26 16:38:41 +0200

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -1,13 +1,11 @@
 sudo pacman -Sy
-sudo pacman --needed base-devel
-sudo pacman python-pip python-virtualenv nodejs npm openjdk r neovim neovim-qt curl wget gimp mpv firefox
+sudo pacman -S --needed base-devel
+sudo pacman -S python-pip python-virtualenv nodejs npm r neovim neovim-qt curl wget gimp mpv firefox
 
 curl -sLf https://spacevim.org/install.sh | bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 git clone https://aur.archlinux.org/rstudio-desktop-bin.git
 
-mkdir .themes
-mkdir .icons
 
 echo "IntelliJ IDEA:  https://www.jetbrains.com/idea/download/#section=linux"
 echo "Tor Browser:  https://www.torproject.org/download/"
