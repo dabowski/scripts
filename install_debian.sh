5f949ce4dd250d039d23a2d2b9aa6d946cab0f2c
dabowski <52606353+dabowski@users.noreply.github.com>
2021-05-27 23:28:31 +0200

    Update install_debian.sh

--- install_debian.sh
+++ install_debian.sh
@@ -1,16 +1,9 @@
 sudo apt update
-sudo apt install gcc python3 python3-pip python3-virtualenv nodejs npm default-jdk r-base neovim neovim-qt curl wget gimp mpv firefox-esr
+sudo apt install gcc python3 python3-pip python3-virtualenv r-base neovim neovim-qt curl gimp mpv firefox
 
 curl -sLf https://spacevim.org/install.sh | bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-
-mkdir .themes
-mkdir .icons
 
 echo "RStudio:  https://www.rstudio.com/products/rstudio/download/#download"
-
-echo "IntelliJ IDEA:  https://www.jetbrains.com/idea/download/#section=linux"
-echo "Tor Browser:  https://www.torproject.org/download/"
 echo "Papirus:  https://www.xfce-look.org/p/1166289/"
 echo "Mojave dark: https://www.xfce-look.org/p/1275087/"
 echo "Mojave cursors: https://www.xfce-look.org/p/1355701/"
