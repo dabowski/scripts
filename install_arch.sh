89c1a59ebe3ae6bee87ae0d9b0f1903acd7dc25f
dabowski <52606353+dabowski@users.noreply.github.com>
2021-06-03 14:53:30 +0200

    Add files via upload

--- install_arch.sh
+++ install_arch.sh
@@ -1,15 +1,12 @@
-sudo pacman -Sy
-sudo pacman -S --needed base-devel
 sudo pacman -S python-pip python-virtualenv
+sudo pacman -S jdk-openjdk
 sudo pacman -S R pandoc-citeproc
-sudo pacman -S neovim neovim-qt
-sudo pacman -S curl
+sudo pacman -S vim 
 sudo pacman -S gimp mpv
 sudo pacman -S firefox
 
-curl -sLf https://spacevim.org/install.sh | bash
-
+echo "Pycharm:  https://www.jetbrains.com/pycharm/download/#section=linux"
+echo "IntelliJ IDEA:  https://www.jetbrains.com/idea/download/#section=linux"
 echo "Tor Browser:  https://www.torproject.org/download/"
 echo "Papirus:  https://www.xfce-look.org/p/1166289/"
 echo "Mojave dark: https://www.xfce-look.org/p/1275087/"
-echo "Mojave cursors: https://www.xfce-look.org/p/1355701/"
