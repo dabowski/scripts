d7d14e02d7ac4ffc626e2df06b9e365489fba52d
dabowski <52606353+dabowski@users.noreply.github.com>
2021-06-03 14:53:44 +0200

    Add files via upload

--- install_debian.sh
+++ install_debian.sh
@@ -1,9 +1,7 @@
 sudo apt update
-sudo apt install gcc python3 python3-pip python3-virtualenv r-base neovim neovim-qt curl gimp mpv firefox
+sudo apt install gcc python3 python3-pip python3-virtualenv r-base openjdk-8-jdk vim gimp mpv firefox
 
-curl -sLf https://spacevim.org/install.sh | bash
 
 echo "RStudio:  https://www.rstudio.com/products/rstudio/download/#download"
 echo "Papirus:  https://www.xfce-look.org/p/1166289/"
 echo "Mojave dark: https://www.xfce-look.org/p/1275087/"
-echo "Mojave cursors: https://www.xfce-look.org/p/1355701/"
