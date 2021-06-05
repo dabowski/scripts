f3b9e0e33209f5292c9bc6ea4f90f37fa3e44060
dabowski <52606353+dabowski@users.noreply.github.com>
2021-06-05 17:08:03 +0200

    Update and rename install_arch.sh to install_pacman.sh

--- install_pacman.sh
+++ install_pacman.sh
@@ -1,12 +1,9 @@
 sudo pacman -S python-pip python-virtualenv
 sudo pacman -S jdk-openjdk
-sudo pacman -S R pandoc-citeproc
 sudo pacman -S vim 
-sudo pacman -S gimp mpv
+sudo pacman -S mpv
 sudo pacman -S firefox
 
-echo "Pycharm:  https://www.jetbrains.com/pycharm/download/#section=linux"
-echo "IntelliJ IDEA:  https://www.jetbrains.com/idea/download/#section=linux"
 echo "Tor Browser:  https://www.torproject.org/download/"
 echo "Papirus:  https://www.xfce-look.org/p/1166289/"
 echo "Mojave dark: https://www.xfce-look.org/p/1275087/"
