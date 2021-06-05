6425f8eb4c68b79f4c05633847a5e065b0f3bb51
dabowski <52606353+dabowski@users.noreply.github.com>
2021-06-05 17:02:26 +0200

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,11 +1,9 @@
 sudo apt update
-sudo apt install gcc python3 python3-pip python3-virtualenv r-base openjdk-8-jdk gimp mpv
+sudo apt install gcc python3 python3-pip python3-virtualenv openjdk-8-jdk mpv
 
 sudo snap install brave
 sudo snap install pycharm-community --classic
-sudo snap install atom --classic
-sudo snap install intelliji-idea-community --classic
+sudo snap install intellij-idea-community --classic
 
-echo "RStudio:  https://www.rstudio.com/products/rstudio/download/#download"
 echo "Papirus:  https://www.xfce-look.org/p/1166289/"
 echo "Mojave dark: https://www.xfce-look.org/p/1275087/"
