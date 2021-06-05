61dded49da0e8587c2d15bd3c1e128dc27e9c0c9
dabowski <52606353+dabowski@users.noreply.github.com>
2021-06-05 17:08:30 +0200

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -7,3 +7,4 @@ sudo snap install intellij-idea-community --classic
 
 echo "Papirus:  https://www.xfce-look.org/p/1166289/"
 echo "Mojave dark: https://www.xfce-look.org/p/1275087/"
+echo "Tor Browser:  https://www.torproject.org/download/"
