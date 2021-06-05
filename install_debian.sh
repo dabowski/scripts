29afd5ba16e8a0afa830c9588530d89cdad71d60
dabowski <52606353+dabowski@users.noreply.github.com>
2021-06-05 17:03:14 +0200

    Update install_debian.sh

--- install_debian.sh
+++ install_debian.sh
@@ -1,7 +1,5 @@
 sudo apt update
-sudo apt install gcc python3 python3-pip python3-virtualenv r-base openjdk-8-jdk vim gimp mpv firefox
+sudo apt install gcc python3 python3-pip python3-virtualenv openjdk-8-jdk vim mpv firefox
 
-
-echo "RStudio:  https://www.rstudio.com/products/rstudio/download/#download"
 echo "Papirus:  https://www.xfce-look.org/p/1166289/"
 echo "Mojave dark: https://www.xfce-look.org/p/1275087/"
