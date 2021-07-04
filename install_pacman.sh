8136b7877fa703d5bfd066d5f913b279f4e38b08
dabowski <52606353+dabowski@users.noreply.github.com>
2021-07-04 15:07:02 +0200

    Delete install_pacman.sh

--- install_pacman.sh
+++
@@ -1,9 +0,0 @@
-sudo pacman -Syu
-
-sudo pacman -S python-pip python-virtualenv
-sudo pacman -S vim
-sudo pacman -S mpv
-sudo pacman -S firefox
-
-echo "Tor Browser:  https://www.torproject.org/download/"
-echo "Dracula Theme:  https://draculatheme.com/gtk"
