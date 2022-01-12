3a95dd3e876bcfe7f5b947ea222de12259ddfde6
dabowski <52606353+dabowski@users.noreply.github.com>
2022-01-12 18:41:15 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 sudo apt update
+sudo apt install xorg xinit i3 lightdm kitty alsa-utils pulseaudio
+sudo apt install python3-pip python3-virtualenv r-base texlive-full pandoc neovim thunar mpv ffmpeg
+sudo apt install firefox
+sudo apt install obs-studio gimp
 
-sudo apt install gcc
-sudo apt install python3 python3-pip python3-virtualenv
-sudo apt install r-base
-sudo apt install neovim mpv
-sudo apt install firefox-esr
+mkdir ~/.config/nvim
+mkdir ~/.config/i3
+mkdir ~/.config/i3status
 
-cd ..
-mkdir .themes
-mkdir .icons
-virtualenv dataScience
+mv .vimrc ~/.config/nvim/init.vim
+cp i3config ~/.config/i3/config
+mv i3statusconfig ~/.config/i3status/config
 
-echo "Dracula Theme:  https://draculatheme.com/gtk"
-echo "Tor Browser:  https://www.torproject.org/download/"
+echo "https://www.blender.org/download/"
+echo "https://www.rstudio.com/products/rstudio/download/"
