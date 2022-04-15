e810d5bfdaab27024d64f759e7e2d4a67b70a517
dabowski <52606353+dabowski@users.noreply.github.com>
2022-04-15 20:39:02 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 sudo apt update
-sudo apt install python3-pip python3-virtualenv texlive-full pandoc neovim gimp 
-sudo apt install gnome-tweaks gnome-shell-extentions
+sudo apt install xorg xinit arandr i3 kitty lxappearance python3-pip python3-virtualenv texlive-core pandoc vim-nox htop zathura zathura-pdf-poppler mpv gimp libreoffice snap ca-certificates curl gnupg lsb-release docker-ce docker-ce-cli containerd.io
 
-mkdir ~/.config/nvim
-
-mv .vimrc ~/.config/nvim/init.vim
+mkdir ~/.config/i3
+mkdir ~/.config/kitty
+mv i3config ~/.config/i3/config
+mv i3status.conf ~/.config/i3status.conf
+mv kitty.conf ~/.config/kitty.conf
+mv .vimrc ~/.vimrc
 mv .bashrc ~/.bashrc
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundleVundle.vim
+git clone https://github.com/dracula/gtk.git ~/.themes/dracula
+git clone https://github.com/m4thewz/dracula-icons ~/.icons/dracula
+
 cd ~ && virtualenv lab
 
-sudo snap install pycharm-community intellij-community --classic
-sudo snap install docker hugo kdenlive obs-studio 
+sudo snap install intellij-idea-community --classic
+sudo snap install kdenlive obs-studio teams
 
-echo "Do not forget to download theme, icons and wallpaper"
