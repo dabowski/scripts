1bbb6f021b1ef32aab22b3b31a530ffc1fa0777e
dabowski <52606353+dabowski@users.noreply.github.com>
2022-04-25 22:11:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,10 @@
+#!/bin/bash
+
 sudo apt update
-sudo apt install xorg xinit arandr i3 kitty lxappearance python3-pip python3-virtualenv texlive-core pandoc vim-nox htop zathura zathura-pdf-poppler mpv gimp libreoffice snap ca-certificates curl gnupg lsb-release docker-ce docker-ce-cli containerd.io
+sudo apt install python3-pip python3-virtualenv r-base texlive-base pandoc vim-nox htop gimp
 
-mkdir ~/.config/i3
-mkdir ~/.config/kitty
-mv i3config ~/.config/i3/config
-mv i3status.conf ~/.config/i3status.conf
-mv kitty.conf ~/.config/kitty.conf
-mv .vimrc ~/.vimrc
-mv .bashrc ~/.bashrc
+mv vimrc ~/.vimrc
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundleVundle.vim
-git clone https://github.com/dracula/gtk.git ~/.themes/dracula
-git clone https://github.com/m4thewz/dracula-icons ~/.icons/dracula
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-cd ~ && virtualenv lab
-
-sudo snap install intellij-idea-community --classic
-sudo snap install kdenlive obs-studio teams
-
+sudo snap install teams
